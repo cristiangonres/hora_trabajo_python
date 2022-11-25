39950027e4cc7e2d1ddf6e432d5126f4503f1003
cristian <gonzalez.resina@gmail.com>
2022-11-25 19:12:30 +0100

    Eliminado print de pruebas

--- work_time.py
+++ work_time.py
@@ -1,15 +1,15 @@
-from datetime import time, datetime
+from datetime import  datetime
 import time
 
 while 1 == 1:
     now_str = datetime.now().time().strftime("%H:%M:%S")
     now = datetime.now()
     hora_actual = now.hour
-    print(hora_actual)
     minuto_actual = now.minute
     segundo_actual = now.second
     if hora_actual >= 19:
         print('Es hora de irse a casa')
+        break
     else:
         print (f'Son las:  {now_str}, te falta {18-hora_actual} HORAS, {59-minuto_actual} MINUTOS, {59-segundo_actual} SEGUNDOS para salir')
     time.sleep(10)
